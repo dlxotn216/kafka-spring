c5920276c3fe03aba109f3d6f5a611c42b7eb298
taesu <taesu@crscube.co.kr>
2024-03-05 22:56:54 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+```shell
+docker-compose up -d --force-recreate
+docker compose down
+```
+
+https://medium.com/@erkndmrl/kafka-cluster-with-docker-compose-5864d50f677e
---
+++ src/main/kotlin/io/taesu/kafkaspring/KafkaSpringApplication.kt
@@ -0,0 +1,11 @@
+package io.taesu.kafkaspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<KafkaSpringApplication>(*args)
+}
